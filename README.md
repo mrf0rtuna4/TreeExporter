de30545e76d2826ccdc0fbf34b6c783f9c366ae9
mrf0rtuna4 <majned90@gmail.com>
2026-08-04 13:54:20 +0300

    feat: project init

---
+++ README.md
@@ -0,0 +1,3 @@
+# TreeExporter
+
+Automatic export of the project structure to SVG format. (W.I.P.)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from treeexporter!")
+
+
+if __name__ == "__main__":
+    main()
